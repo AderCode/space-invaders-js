125f72592f3fb67709100d4595fc2c438b657b1a
Matthew Aderhold <matthewaderhold@gmail.com>
2018-04-22 13:25:13 -0500

    starfield complete

--- js/starfield.js
+++ js/starfield.js
@@ -12,7 +12,7 @@ class Starfield {
     }
 
     init(div) {
-        self = this;
+        let self = this;
         this.containerDiv = div;
         self.width = window.innerWidth;
         self.height = window.innerHeight;
@@ -31,19 +31,61 @@ class Starfield {
         this.canvas.width = this.width;
         this.canvas.height = this.height;
     }
-    
+
     start() {
         let stars = [];
-        for(let i = 0; i < this.stars; i++){
-            stars[i] = new Star(Math.random()*this.width, Math.random()*3+1, (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity);
+        for (let i = 0; i < this.stars; i++) {
+            stars[i] = new Star(
+                Math.random() * this.width,
+                Math.random() * this.height,
+                Math.random() * 3 + 1,
+                (Math.random() * (this.maxVelocity - this.minVelocity)) + this.minVelocity
+            );
         }
         this.stars = stars;
-    }
+        let self = this;
+        this.intervalId = setInterval(() => {
+            self.update();
+            self.draw();
+        }, 1000 / this.fps);
+    };
 
+    update() {
+        let dt = 1 / this.fps;
+        for (let i = 0; i < this.stars.length; i++) {
+            let star = this.stars[i];
+            star.y += dt * star.velocity;
+            if (star.y > this.height) {
+                this.stars[i] = new Star(Math.random() * this.width, 0, Math.random() * 3 + 1, (Math.random() * (this.maxVelocity - this.minVelocity)) + this.minVelocity);
+            }
+        }
+    };
+
+    draw() {
+        let ctx = this.canvas.getContext("2d");
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, this.width, this.height);
+
+        ctx.fillStyle = '#ffffff';
+        for (let i = 0; i < this.stars.length; i++) {
+            let star = this.stars[i];
+            ctx.fillRect(star.x, star.y, star.size, star.size);
+        }
+    }
 }
 
 class Star {
     constructor(x, y, size, velocity) {
-        
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.velocity = velocity;
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    let container = document.getElementById('container');
+    let starfield = new Starfield;
+    starfield.init(container);
+    starfield.start();
+});
